261fdcc4622ca3baafe095084b0390811a30cf79
J-Dany <jdanya@outlook.com>
2021-03-17 09:15:24 +0100

    Scritta la classe DatabaseTable che rappresenta appunto una tabella del database

--- classes/DatabaseTable.php
+++ classes/DatabaseTable.php
@@ -3,7 +3,123 @@
 // TODO
 class DatabaseTable
 {
-    protected array $columns;
-    protected string $pk;
-    protected string $name;
+    protected $columns;
+    protected $pk;
+    protected $name;
+    protected $result;
+    protected $connection;
+    private $insertQuery;
+
+    public function __construct (\mysqli $connection, string $pk, string $name)
+    {
+        $this->$connection = $connection;
+        $this->name = $name;
+        $this->pk = $pk;
+
+        $this->settaArrayInserimento();
+        $this->connection->autocommit(false);
+        $this->settaArrayColonne();
+    }
+
+    public function seleziona ($pk) : \mysqli_result
+    {
+        $query = "SELECT * FROM '$this->name' WHERE '$this->pk' = '$pk'";
+
+        $result = $this->connection->query($query);
+        $this->result = $result;
+
+        return $result;
+    }
+
+    public function selezionaTutto () : \mysqli_result
+    {
+        $query = "SELECT * FROM '$this->name'";
+
+        $result = $this->connection->query($query);
+        $this->result = $result;
+
+        return $result;
+    }
+
+    public function inserisci (array $values) : void
+    {
+        try
+        {
+            $stmt = $this->connection->prepare($this->insertQuery);
+
+            foreach ($values as $value)
+            {
+                if (is_int($value))
+                {
+                    $stmt->bind_param("i", $value);
+                }
+                else if (is_string($value))
+                {
+                    $stmt->bind_param("s", $value);
+                }
+                else if (is_float($value))
+                {
+                    $stmt->bind_param("d", $value);
+                }
+            }
+
+            $stmt->execute();
+
+            $this->connection->commit();
+        }
+        catch (\Exception $e)
+        {
+            $this->connection->rollback();
+        }
+    }
+
+    public function elimina ($pk) : void
+    {
+        try
+        {
+            $query = "DELETE FROM '$this->name' WHERE '$this->pk' = '$pk'";
+
+            $this->connection->query($query);
+
+            $this->connection->commit();
+        }
+        catch (\Exception $e)
+        {
+            $this->connection->rollback();
+        }
+    }
+
+    // TODO
+    public function aggiorna ($pk) : void
+    {
+
+    }
+
+    private function settaArrayColonne () : void
+    {
+        $query = "SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '$this->name';";
+
+        $this->columns = $this->connection->query($query)->fetch_array();
+    }
+
+    private function settaArrayInserimento ()
+    {
+        $query = "INSERT INTO '$this->name'(";
+
+        foreach ($this->columns as $column)
+        {
+            $query .= "$column,";
+        }
+
+        $query = rtrim($query, ",") . ") VALUES (";
+
+        foreach ($this->columns as $column)
+        {
+            $query .= "?, ";
+        }
+
+        $query = rtrim($query, ",") . ")";
+        
+        $this->insertQuery = $query;
+    }
 };
--- classes/Model/Model.php
+++ classes/Model/Model.php
@@ -4,142 +4,15 @@ namespace Model;
 
 abstract class Model
 {
-    protected array $columns;
-    protected string $pk;
-    protected string $name;
-    protected \mysqli_result $result;
-    protected \mysqli $connection;
-
-    private string $insertQuery;
+    protected $dbtable;
+    protected $result;
 
     public function __construct (\mysqli $connection, string $name, string $pk)
     {
-        $this->connection = $connection;
-        $this->name = $name;
-        $this->pk = $pk;
-
-        $this->settaArrayColonne();
-        $this->connection->autocommit(false);
-        $this->settaQueryInserimento();
+        $this->dbtable = new \DatabaseTable($connection, $name, $pk);
     }
 
-    protected function prendi ($pk) : \mysqli_result
-    {
-        $query = "SELECT * FROM '$this->name' WHERE '$this->pk' = '$pk'";
-
-        $result = $this->connection->query($query);
-        $this->result = $result;
-
-        return $result;
-    }
-
-    protected function prendiTutto () : \mysqli_result
-    {
-        $query = "SELECT * FROM '$this->name'";
-
-        $result = $this->connection->query($query);
-        $this->result = $result;
-
-        return $result;
-    }
-
-    protected function inserisci (array $values) : void
-    {
-        try
-        {
-            $stmt = $this->connection->prepare($this->insertQuery);
-
-            foreach ($values as $value)
-            {
-                if (is_int($value))
-                {
-                    $stmt->bind_param("i", $value);
-                }
-                else if (is_string($value))
-                {
-                    $stmt->bind_param("s", $value);
-                }
-                else if (is_float($value))
-                {
-                    $stmt->bind_param("d", $value);
-                }
-            }
-
-            $stmt->execute();
-
-            $this->connection->commit();
-        }
-        catch (\Exception $e)
-        {
-            $this->connection->rollback();
-        }
-    }
-
-    protected function elimina ($pk) : void
-    {
-        try
-        {
-            $query = "DELETE FROM '$this->name' WHERE '$this->pk' = '$pk'";
-
-            $this->connection->query($query);
-
-            $this->connection->commit();
-        }
-        catch (\Exception $e)
-        {
-            $this->connection->rollback();
-        }
-    }
-
-    // TODO
-    protected function aggiorna ($pk) : void
-    {
-
-    }
-
-    protected function api_prendi ($pk = null) : void
-    {
-        $query = "";
-
-        if (is_null($pk))
-        {
-            $query = "SELECT * FROM OBSERVATIONS";
-        }
-        else
-        {
-            $query = "SELECT * FROM OBSERVATIONS WHERE time = '$pk'";
-        }
-
-        $this->result = $this->connection->query($query);
-    }
-
-    private function settaArrayColonne () : void
-    {
-        $query = "SELECT column_name FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '$this->name';";
-
-        $this->columns = $this->connection->query($query)->fetch_array();
-    }
-
-    private function settaQueryInserimento ()
-    {
-        $query = "INSERT INTO '$this->name'(";
-
-        foreach ($this->columns as $column)
-        {
-            $query .= "$column,";
-        }
-
-        $query = rtrim($query, ",") . ") VALUES (";
-
-        foreach ($this->columns as $column)
-        {
-            $query .= "?, ";
-        }
-
-        $query = rtrim($query, ",") . ")";
-        
-        $this->insertQuery = $query;
-    }
+    public abstract function prendi ($pk = null) : void;
 
     public function xml () : string
     {
